9befe77c1142edcb1a8707afd716aa5bf45c71f0
Documenter.jl <documenter@juliadocs.github.io>
2024-09-19 20:57:34 +0000

    build based on ca3976f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyCoolPackage","category":"page"},{"location":"#MyCoolPackage","page":"Home","title":"MyCoolPackage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyCoolPackage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyCoolPackage]","category":"page"}]
+}
